4f510e64d78646f692762884ec6b9b0ff39c6d5b
Lucas Crownover <lcrownover127@gmail.com>
2024-06-14 08:08:16 +0100

    update from none-ls

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,149 +1,154 @@
 -- LSP and LSP accessories
 return {
-	"neovim/nvim-lspconfig",
-	event = { "BufRead", "BufNewFile" },
-	opts = {
-		inlay_hints = { enable = true },
-	},
-	dependencies = {
-		{ "j-hui/fidget.nvim", opts = {} },
-		"williamboman/mason.nvim",
-		"williamboman/mason-lspconfig.nvim",
-		"WhoIsSethDaniel/mason-tool-installer.nvim",
-		{
-			"folke/neodev.nvim",
-			opts = {},
-		},
-	},
-	config = function()
-		vim.api.nvim_create_autocmd("LspAttach", {
-			group = vim.api.nvim_create_augroup("attach-lsp", { clear = true }),
-			callback = function(event)
+    "neovim/nvim-lspconfig",
+    event = { "BufRead", "BufNewFile" },
+    opts = {
+        inlay_hints = { enable = true },
+    },
+    dependencies = {
+        { "j-hui/fidget.nvim", opts = {} },
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
+        "WhoIsSethDaniel/mason-tool-installer.nvim",
+        {
+            "folke/neodev.nvim",
+            opts = {},
+        },
+    },
+    config = function()
+        vim.api.nvim_create_autocmd("LspAttach", {
+            group = vim.api.nvim_create_augroup("attach-lsp", { clear = true }),
+            callback = function(event)
                 local toggle_inlay_hints = function()
                     vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({}))
                 end
-				local map = function(keys, func, desc)
-					vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
-				end
-				map("gd", require("telescope.builtin").lsp_definitions, "Goto Definition")
-				map("gD", vim.lsp.buf.declaration, "Goto Declaration")
-				map("gr", require("telescope.builtin").lsp_references, "Goto References")
-				map("gi", require("telescope.builtin").lsp_implementations, "Goto Implementations")
-				map("<leader>ls", require("telescope.builtin").lsp_document_symbols, "Document Symbols")
-				map("<leader>lr", ":LspRestart<cr>", "Restart LSP server")
-				map("<leader>li", ":LspInfo<cr>", "Show LSP info")
-				map("<leader>lh", toggle_inlay_hints, "Toggle LSP inlay hints")
-				map("<leader>fs", vim.lsp.buf.format, "LSP Format")
-				-- map("gn", vim.lsp.buf.rename, "Rename Symbol")
-				map("<F2>", vim.lsp.buf.rename, "Rename Symbol")
-				map("ca", vim.lsp.buf.code_action, "Code Action")
-			end,
-		})
+                local map = function(keys, func, desc)
+                    vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
+                end
+                map("gd", require("telescope.builtin").lsp_definitions, "Goto Definition")
+                map("gD", vim.lsp.buf.declaration, "Goto Declaration")
+                map("gr", require("telescope.builtin").lsp_references, "Goto References")
+                map("gi", require("telescope.builtin").lsp_implementations, "Goto Implementations")
+                map("<leader>ls", require("telescope.builtin").lsp_document_symbols, "Document Symbols")
+                map("<leader>lr", ":LspRestart<cr>", "Restart LSP server")
+                map("<leader>li", ":LspInfo<cr>", "Show LSP info")
+                map("<leader>lh", toggle_inlay_hints, "Toggle LSP inlay hints")
+                map("<leader>fs", vim.lsp.buf.format, "LSP Format")
+                -- map("gn", vim.lsp.buf.rename, "Rename Symbol")
+                map("<F2>", vim.lsp.buf.rename, "Rename Symbol")
+                map("ca", vim.lsp.buf.code_action, "Code Action")
+            end,
+        })
 
-		-- Advertise LSP capabilities
-		local capabilities = vim.lsp.protocol.make_client_capabilities()
-		capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
+        -- Advertise LSP capabilities
+        local capabilities = vim.lsp.protocol.make_client_capabilities()
+        capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
 
-		-- LSP servers and their configurations
-		local servers = {
-			bashls = {},
-			html = {},
-			marksman = {},
-			pyright = {
-				settings = {
-					python = {
-						analysis = {
-							autoImportCompletions = false,
-						},
-					},
-				},
-			},
-			ansiblels = {
-				filetypes = {
-					"yml.ansible",
-					"yml",
-					"yaml",
-				},
-			},
-			gopls = {
-				settings = {
-					gopls = {
-						staticcheck = true,
-					},
-				},
-			},
-			rust_analyzer = {
-				tools = {
-					inlay_hints = {
-						auto = false,
-					},
-				},
-			},
-			terraformls = {
-				filetypes = {
-					"terraform",
-					"tf",
-				},
-			},
-			svelte = {},
-			lua_ls = {
-				settings = {
-					Lua = {
-						completion = {
-							callSnippet = "Replace",
-						},
-						diagnostics = {
-							disable = { "missing-fields" },
-							globals = { "vim" },
-						},
-					},
-				},
-			},
-		}
+        -- LSP servers and their configurations
+        local servers = {
+            bashls = {},
+            html = {},
+            marksman = {},
+            pyright = {
+                settings = {
+                    python = {
+                        analysis = {
+                            autoImportCompletions = false,
+                        },
+                    },
+                },
+            },
+            ansiblels = {
+                filetypes = {
+                    "yml.ansible",
+                    "yml",
+                    "yaml",
+                },
+            },
+            gopls = {
+                settings = {
+                    gopls = {
+                        staticcheck = true,
+                    },
+                },
+            },
+            rust_analyzer = {
+                tools = {
+                    inlay_hints = {
+                        auto = false,
+                    },
+                },
+            },
+            terraformls = {
+                filetypes = {
+                    "terraform",
+                    "tf",
+                },
+            },
+            svelte = {},
+            lua_ls = {
+                settings = {
+                    Lua = {
+                        completion = {
+                            callSnippet = "Replace",
+                        },
+                        diagnostics = {
+                            disable = { "missing-fields" },
+                            globals = { "vim" },
+                        },
+                    },
+                },
+            },
+        }
 
-		-- List of formatters and linters to install
-		local lsp_tools = {
-			"prettier",
-			"prettierd",
-			"stylua",
-			"isort",
-			"ruff",
-			"eslint_d",
-			"ansible-lint",
-			"shfmt",
-			"shellcheck",
-			"markdownlint",
-			"yamlfmt",
-		}
+        -- List of formatters and linters to install
+        local lsp_tools = {
+            "prettier",
+            "prettierd",
+            "stylua",
+            "isort",
+            "ruff",
+            "mypy",
+            "eslint_d",
+            "ansible-lint",
+            "shfmt",
+            "shellcheck",
+            "markdownlint",
+            "vale",
+            "yamlfmt",
+        }
 
-		-- Builds a list of tools (LSP servers, formatters, linters) to be installed by mason-tool-installer
-		local ensure_installed = vim.tbl_keys(servers or {})
-		vim.list_extend(ensure_installed, lsp_tools)
+        -- Builds a list of tools (LSP servers, formatters, linters) to be installed by mason-tool-installer
+        local ensure_installed = vim.tbl_keys(servers or {})
+        vim.list_extend(ensure_installed, lsp_tools)
 
-		-- Initialize Mason for installing tools
-		require("mason").setup({
-			ui = {
-				icons = {
-					package_installed = "✓",
-					package_pending = "➜",
-					package_uninstalled = "✗",
-				},
-			},
-		})
+        -- Initialize Mason for installing tools
+        require("mason").setup({
+            ui = {
+                icons = {
+                    package_installed = "✓",
+                    package_pending = "➜",
+                    package_uninstalled = "✗",
+                },
+            },
+        })
 
-		-- Ensure all LSP servers are installed
-		require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
+        -- Ensure all LSP servers are installed
+        require("mason-tool-installer").setup({
+            ensure_installed = ensure_installed,
+            run_on_start = true,
+        })
 
-		-- Set up all the LSP servers using the language-specific config blocks above
-		-- This automatically sets capabilities for each server
-		require("mason-lspconfig").setup({
-			handlers = {
-				function(server_name)
-					local server = servers[server_name] or {}
-					server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
-					require("lspconfig")[server_name].setup(server)
-				end,
-			},
-		})
-	end,
+        -- Set up all the LSP servers using the language-specific config blocks above
+        -- This automatically sets capabilities for each server
+        require("mason-lspconfig").setup({
+            handlers = {
+                function(server_name)
+                    local server = servers[server_name] or {}
+                    server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
+                    require("lspconfig")[server_name].setup(server)
+                end,
+            },
+        })
+    end,
 }
---
+++ lua/plugins/nvim-lint.lua
@@ -0,0 +1,20 @@
+return {
+	"mfussenegger/nvim-lint",
+	event = "BufRead",
+	config = function()
+		require("lint").linters_by_ft = {
+			ansible = { "ansible_lint" },
+			yaml = { "ansible_lint", "yamllint" },
+			python = { "mypy", "ruff" },
+			lua = { "luacheck" },
+			sh = { "shellcheck" },
+			markdown = { "vale" },
+		}
+
+		vim.api.nvim_create_autocmd({ "InsertLeave" }, {
+			callback = function()
+				require("lint").try_lint()
+			end,
+		})
+	end,
+}
