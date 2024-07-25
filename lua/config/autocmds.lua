8cc21df14592538bbf8ef877e9fae0ff89e2ee06
Lucas Crownover <lcrownover127@gmail.com>
2024-07-25 14:25:12 -0700

    terminal stuff

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,11 +1,8 @@
 -- this flashes the current selection when I yank it
 vim.api.nvim_create_autocmd("TextYankPost", {
-	desc = "Highlight when yanking text",
-	group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
-	callback = function()
-		vim.highlight.on_yank({
-			-- higroup = "IncSearch",
-			-- timeout = 100,
-		})
-	end,
+    desc = "Highlight when yanking text",
+    group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
+    callback = function()
+        vim.highlight.on_yank({})
+    end,
 })
