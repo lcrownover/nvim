0b048a8d1fd5ab2b40c1b0d38eb5743fb43900f5
Lucas Crownover <lcrownover127@gmail.com>
2024-11-25 20:19:34 -0800

    ufo for folding

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -50,7 +50,7 @@ return {
             clangd = {},
             html = {},
             marksman = {},
-            ruff_lsp = {
+            ruff = {
                 -- settings = {
                 --     python = {
                 --         analysis = {
