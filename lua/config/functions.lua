4eae96ef6d0413d22b1249b2a33332c290f26ec0
Lucas Crownover <lcrownover127@gmail.com>
2024-12-16 11:45:17 -0800

    formatting?

--- lua/config/functions.lua
+++ lua/config/functions.lua
@@ -1,39 +1,39 @@
 -- this will toggle the visibility of whitespace characters
 function ToggleWhitespaceVisibility()
-	vim.opt.list = not vim.opt.list:get()
+    vim.opt.list = not vim.opt.list:get()
 end
 
 -- this uses the LSP formatter if it exists,
 -- otherwise uses conform.nvim
 function Format()
-	-- get the first lsp client
-	-- if you have multiple lsp clients attached to the buffer then you need to handle that case
-	local client = vim.lsp.get_clients({ bufnr = 0 })[1]
-	if client and client.supports_method("textDocument/formatting") then
-		vim.lsp.buf.format()
-	else
-		require("conform").format({ bufnr = 0 })
-	end
+    -- get the first lsp client
+    -- if you have multiple lsp clients attached to the buffer then you need to handle that case
+    local client = vim.lsp.get_clients({ bufnr = 0 })[1]
+    if client and client.supports_method("textDocument/formatting") then
+        vim.lsp.buf.format()
+    else
+        require("conform").format({ bufnr = 0 })
+    end
 end
 
 function GoSetBuildTags()
-	local client_id = 1
-	local client = vim.lsp.get_client_by_id(client_id)
-	if client and client.name == "gopls" then
-		vim.ui.input({ prompt = "Enter build tags: " }, function(input)
-			if input then
-				client.config.settings.gopls.buildFlags = { "-tags=" .. input }
-				client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
-				print("Updated gopls buildFlags to: -tags=" .. input)
-			else
-				print("No input provided")
-			end
-		end)
-	else
-		print("gopls client not found")
-	end
+    local client_id = 1
+    local client = vim.lsp.get_client_by_id(client_id)
+    if client and client.name == "gopls" then
+        vim.ui.input({ prompt = "Enter build tags: " }, function(input)
+            if input then
+                client.config.settings.gopls.buildFlags = { "-tags=" .. input }
+                client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
+                print("Updated gopls buildFlags to: -tags=" .. input)
+            else
+                print("No input provided")
+            end
+        end)
+    else
+        print("gopls client not found")
+    end
 end
 
 function ToggleInlayHints()
-	vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({}))
+    vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({}))
 end
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,68 +1,68 @@
 -- LSP and LSP accessories
 return {
-	"neovim/nvim-lspconfig",
-	event = { "BufRead", "BufNewFile" },
-	opts = {
-		-- inlay_hints = { enable = true },
-		servers = {
-			bashls = {},
-			html = {},
-			marksman = {},
-			ruff = {},
+    "neovim/nvim-lspconfig",
+    event = { "BufRead", "BufNewFile" },
+    opts = {
+        -- inlay_hints = { enable = true },
+        servers = {
+            bashls = {},
+            html = {},
+            marksman = {},
+            ruff = {},
             ansiblels = {
                 filetypes = {
                     "ansible",
                 },
             },
-			gopls = {
-				settings = {
-					gopls = {
-						staticcheck = true,
-						buildFlags = { "-tags=2311" }
-					},
-				},
-			},
-			rust_analyzer = {
-				tools = {
-					inlay_hints = {
-						auto = false,
-					},
-				},
-			},
-			terraformls = {
-				filetypes = {
-					"terraform",
-					"tf",
-				},
-			},
-			svelte = {},
-			lua_ls = {
-				settings = {
-					Lua = {
-						format = { enable = true },
-						completion = {
-							callSnippet = "Replace",
-						},
-						diagnostics = {
-							disable = { "missing-fields" },
-							globals = { "vim" },
-						},
-					},
-				},
-			},
-		}
-	},
-	dependencies = {
-		{ "j-hui/fidget.nvim", opts = {} },
-		{ "folke/neodev.nvim", opts = {} },
-		"williamboman/mason.nvim",
-		"williamboman/mason-lspconfig.nvim",
-		"WhoIsSethDaniel/mason-tool-installer.nvim",
-		'saghen/blink.cmp',
-	},
-	config = function(_, opts)
-		-- Set up neodev first
-		require('neodev').setup({})
+            gopls = {
+                settings = {
+                    gopls = {
+                        staticcheck = true,
+                        buildFlags = { "-tags=2311" }
+                    },
+                },
+            },
+            rust_analyzer = {
+                tools = {
+                    inlay_hints = {
+                        auto = false,
+                    },
+                },
+            },
+            terraformls = {
+                filetypes = {
+                    "terraform",
+                    "tf",
+                },
+            },
+            svelte = {},
+            lua_ls = {
+                settings = {
+                    Lua = {
+                        format = { enable = true },
+                        completion = {
+                            callSnippet = "Replace",
+                        },
+                        diagnostics = {
+                            disable = { "missing-fields" },
+                            globals = { "vim" },
+                        },
+                    },
+                },
+            },
+        }
+    },
+    dependencies = {
+        { "j-hui/fidget.nvim", opts = {} },
+        { "folke/neodev.nvim", opts = {} },
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
+        "WhoIsSethDaniel/mason-tool-installer.nvim",
+        'saghen/blink.cmp',
+    },
+    config = function(_, opts)
+        -- Set up neodev first
+        require('neodev').setup({})
 
         -- Any .yml file is treated as though it's Ansible
         vim.filetype.add({
@@ -71,50 +71,50 @@ return {
             }
         })
 
-		local ensure_installed = {
-			-- LSP Servers
-			"bashls",
-			"clangd",
-			"html",
-			"marksman",
-			"ruff",
-			"ansiblels",
-			"gopls",
-			"rust_analyzer",
-			"terraformls",
-			"svelte",
-			"lua_ls",
-			-- Tools
-			"prettier",
-			"prettierd",
-			"stylua",
-			"luacheck",
-			"isort",
-			"mypy",
-			"ruff",
-			"eslint_d",
-			"ansible-lint",
-			"shfmt",
-			"shellcheck",
-			"markdownlint",
-			"vale",
-			"yamlfix",
-			"yamllint",
-		}
+        local ensure_installed = {
+            -- LSP Servers
+            "bashls",
+            "clangd",
+            "html",
+            "marksman",
+            "ruff",
+            "ansiblels",
+            "gopls",
+            "rust_analyzer",
+            "terraformls",
+            "svelte",
+            "lua_ls",
+            -- Tools
+            "prettier",
+            "prettierd",
+            "stylua",
+            "luacheck",
+            "isort",
+            "mypy",
+            "ruff",
+            "eslint_d",
+            "ansible-lint",
+            "shfmt",
+            "shellcheck",
+            "markdownlint",
+            "vale",
+            "yamlfix",
+            "yamllint",
+        }
 
-		require("mason").setup({})
+        require("mason").setup({})
 
-		require("mason-tool-installer").setup({
-			ensure_installed = ensure_installed,
-			run_on_start = true,
-		})
+        require("mason-tool-installer").setup({
+            ensure_installed = ensure_installed,
+            run_on_start = true,
+        })
 
-		local lspconfig = require("lspconfig")
-		for server, config in pairs(opts.servers) do
-			local lsp_capabilities = vim.lsp.protocol.make_client_capabilities()
-			config.capabilties = vim.tbl_deep_extend("force", lsp_capabilities,
-				require('blink.cmp').get_lsp_capabilities(config.capabilities))
-			lspconfig[server].setup(config)
-		end
-	end
+        local lspconfig = require("lspconfig")
+        for server, config in pairs(opts.servers) do
+            local lsp_capabilities = vim.lsp.protocol.make_client_capabilities()
+            config.capabilties = vim.tbl_deep_extend("force", lsp_capabilities,
+                require('blink.cmp').get_lsp_capabilities(config.capabilities))
+            lspconfig[server].setup(config)
+        end
+    end
 }
