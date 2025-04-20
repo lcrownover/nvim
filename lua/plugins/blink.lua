ee1ddd3feb5c98d4db62f352314ea934d8360476
Lucas Crownover <lcrownover127@gmail.com>
2025-04-20 11:45:54 -0700

    added extra item detail for blink

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -11,6 +11,20 @@ return {
     sources = {
       default = { 'lsp', 'path', 'snippets', 'buffer' },
     },
+    completion = {
+      documentation = { auto_show = false },
+      menu = {
+        draw = {
+          components = {
+            label_description = {
+              text = function(ctx)
+                return ctx.label_description ~= '' and ctx.label_description or ctx.item.detail
+              end,
+            },
+          },
+        },
+      }
+    },
   },
   opts_extend = { "sources.default" },
 }
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,68 +1,67 @@
 -- LSP and LSP accessories
 return {
-  "neovim/nvim-lspconfig",
-  event = { "BufRead", "BufNewFile" },
-  dependencies = {
-    { "j-hui/fidget.nvim", opts = {} },
-    'saghen/blink.cmp',
-  },
-  config = function()
-    -- Any .yml file is treated as though it's Ansible
-    vim.filetype.add({
-      extension = {
-        yml = "ansible"
-      }
-    })
+	"neovim/nvim-lspconfig",
+	event = { "BufRead", "BufNewFile" },
+	dependencies = {
+		{ "j-hui/fidget.nvim", opts = {} },
+		"saghen/blink.cmp",
+	},
+	config = function()
+		-- Any .yml file is treated as though it's Ansible
+		vim.filetype.add({
+			extension = {
+				yml = "ansible",
+			},
+		})
 
-    -- Now configure LSP servers
-    local servers = {
-      bashls = {},
-      html = {},
-      marksman = {},
-      pyright = {},
-      ansiblels = {
-        filetypes = {
-          "ansible"
-        }
-      },
-      gopls = {
-        settings = {
-          gopls = {
-            staticcheck = true,
-            -- buildFlags = { "-tags=2311" }
-          }
-        },
-      },
-      rust_analyzer = {
-        tools = {
-          inlay_hints = {
-            auto = false
-          }
-        },
-      },
-      terraformls = {},
-      svelte = {},
-      lua_ls = {
-        settings = {
-          Lua = {
-            completion = {
-              callSnippet = "Replace",
-            },
-            diagnostics = {
-              disable = { "missing-fields" },
-              globals = { "vim", "Format", "Snacks", "ToggleWhitespaceVisibility" },
-            },
-            runtime = { version = 'LuaJIT' },
-            workspace = { checkThirdParty = false, library = { vim.env.VIMRUNTIME } }
-          },
-        },
-      },
+		-- Now configure LSP servers
+		local servers = {
+			bashls = {},
+			html = {},
+			marksman = {},
+			pyright = {},
+			ansiblels = {
+				filetypes = {
+					"ansible",
+				},
+			},
+			gopls = {
+				settings = {
+					gopls = {
+						staticcheck = true,
+						-- buildFlags = { "-tags=2311" }
+					},
+				},
+			},
+			rust_analyzer = {
+				tools = {
+					inlay_hints = {
+						auto = false,
+					},
+				},
+			},
+			terraformls = {},
+			svelte = {},
+			lua_ls = {
+				settings = {
+					Lua = {
+						completion = {
+							callSnippet = "Replace",
+						},
+						diagnostics = {
+							disable = { "missing-fields" },
+							globals = { "vim", "Format", "Snacks", "ToggleWhitespaceVisibility" },
+						},
+						runtime = { version = "LuaJIT" },
+						workspace = { checkThirdParty = false, library = { vim.env.VIMRUNTIME } },
+					},
+				},
+			},
+		}
 
-    }
-
-    for server, config in pairs(servers) do
-      config.capabilities = require('blink.cmp').get_lsp_capabilities(config.capabilities)
-      require("lspconfig")[server].setup(config)
-    end
-  end
+		for server, config in pairs(servers) do
+			config.capabilities = require("blink.cmp").get_lsp_capabilities(config.capabilities)
+			require("lspconfig")[server].setup(config)
+		end
+	end,
 }
